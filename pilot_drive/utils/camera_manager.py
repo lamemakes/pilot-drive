3ea9af314947aca4c4a2124dd320eb7a6a4d2325
lame <lamemakes@gmail.com>
2022-06-02 20:42:36 -0400

    Small camera fix

--- pilot_drive/utils/camera_manager.py
+++ pilot_drive/utils/camera_manager.py
@@ -10,16 +10,18 @@ import time
 class CameraManager:
     GPIO.setwarnings(False) # Ignore warning for now
     GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-    GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
     def __init__(self, btn_pin):
         self.log = logging.getLogger()
 
         # Create initial state of button
         self.camera_state = False
+
+        # Set button pin to be an input pin and set initial value to be pulled low (off)
+        GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         
-        # Create GPIO event for the backup camera
-        GPIO.add_event_detect(btn_pin, GPIO.RISING, callback=self.show_camera)
+        # Create GPIO event for the backup camera, debounce is needed for the button in my experience.
+        GPIO.add_event_detect(btn_pin, GPIO.RISING, callback=self.show_camera, bouncetime=400)
 
         # Initialize the camera
         try:
@@ -29,12 +31,12 @@ class CameraManager:
 
     def show_camera(self, channel):
         self.camera_state = not self.camera_state
+        self.log.debug("Camera state set to: " + str(self.camera_state))
         if self.camera_state:
             self.camera.start_preview()
         else:
             self.camera.stop_preview()
-    
-        time.sleep(3)
+
 
 if __name__ == "__main__":
     backup_cam = CameraManager(16)
